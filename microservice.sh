df2036ce42d53509f99da3cfe02a724d581d12e3
daddychuks <durugo_chuks@yahoo.com>
2018-05-21 18:33:14 +0100

    update cluster creation

--- microservice.sh
+++ microservice.sh
@@ -29,11 +29,13 @@ export KOPS_STATE_STORE=s3://$BUCKET_NAME
 
 createCluster(){
 echo ----------------- Creating Cluster ------------------------
-# kops create cluster --name $NAME --master-count 1 --node-count 2 --node-size t2.micro --master-size t2.micro --zones $ZONES --networking kubenet --kubernetes-version v1.8.4 --yes --image "099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180405"
-kops create cluster --name $NAME --node-count 2 --node-size \
-t2.micro --master-size t2.micro --zones us-east-2b --dns private \
---topology private --vpc $vpc_id --networking weave --bastion \
---kubernetes-version v1.8.4 --yes --image "099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180405"
+kops create cluster --name $NAME --master-count 1 --node-count 2 \
+--node-size t2.micro --cloud aws --master-size t2.micro --zones us-east-2b \
+--networking kubenet --kubernetes-version v1.8.4 --yes --image "099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180405"
+# kops create cluster --name $NAME --node-count 2 --node-size \
+# t2.micro --master-size t2.micro --zones us-east-2b --dns private \
+# --topology private --vpc $vpc_id --networking weave --bastion \
+# --kubernetes-version v1.8.4 --yes --image "099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180405"
 }
 
 buildCluster(){
